02b162975834938cfd3d4e64c95f829a8377e08d
lol <lol@lols-mbp.home>
2020-03-16 20:20:36 -0400

    parse arguments, prompt user, and print results in separate functions

--- Program.cs
+++ Program.cs
@@ -2,21 +2,59 @@
 
 namespace FizzBuzz
 {
-    class FizzBuzzInput
+    class FizzBuzz
     {
-        public void CustomerInput(int LowerBound, int UpperBound, int Fizz, int Buzz)
+        public int lowerBound = 1;
+        private string rawLowerBound;
+        public int upperBound = 100;
+        private string rawUpperBound;
+        public int customFizz = 3;
+        private string rawCustomFizz;
+        public int customBuzz = 5;
+        private string rawCustomBuzz;
+        private void ParseArgs()
         {
-            for (int i = LowerBound; i <= UpperBound; i++)
+            this.lowerBound = Int32.Parse(this.rawLowerBound);
+            this.upperBound = Int32.Parse(this.rawUpperBound);
+            this.customFizz = Int32.Parse(this.rawCustomFizz);
+            this.customBuzz = Int32.Parse(this.rawCustomBuzz);
+        }
+        private void CustomBoundQuestions()
+        {
+            System.Console.WriteLine("Enter lower bound:");
+            this.rawLowerBound = System.Console.ReadLine();
+            System.Console.WriteLine("Enter upper bound:");
+            this.rawUpperBound = System.Console.ReadLine();
+        }
+
+        private void CustomFizzBuzzQuestions()
+        {
+            System.Console.WriteLine("Enter Fizz:");
+            this.rawCustomFizz = System.Console.ReadLine();
+            System.Console.WriteLine("Enter Buzz:");
+            this.rawCustomBuzz = System.Console.ReadLine();
+        }
+
+        public void PromptUser()
+        {
+            this.CustomBoundQuestions();
+            this.CustomFizzBuzzQuestions();
+        }
+
+        private void OutputResults()
+        {
+            this.ParseArgs();
+            for (int i = this.lowerBound; i <= this.upperBound; i++)
             {
-                if (i % Fizz == 0 && i % Buzz == 0)
+                if (i % this.customFizz == 0 && i % this.customBuzz == 0)
                 {
                     System.Console.WriteLine("FizzBuzz");
                 }
-                else if (i % Fizz == 0)
+                else if (i % this.customFizz == 0)
                 {
                     System.Console.WriteLine("Fizz");
                 }
-                else if (i % Buzz == 0)
+                else if (i % this.customFizz == 0)
                 {
                     System.Console.WriteLine("Buzz");
                 }
@@ -26,22 +64,19 @@ namespace FizzBuzz
                 }
             }
         }
+        public void PrintResults()
+        {
+            this.ParseArgs();
+            this.OutputResults();
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            FizzBuzzInput input = new FizzBuzzInput();
-            System.Console.WriteLine("Enter lower bound:");
-            var lowerBound = System.Console.ReadLine();
-            System.Console.WriteLine("Enter upper bound:");
-            var upperBound = System.Console.ReadLine();
-            System.Console.WriteLine("Enter Fizz:");
-            var fizz = System.Console.ReadLine();
-            System.Console.WriteLine("Enter Buzz:");
-            var buzz = System.Console.ReadLine();
-            input.CustomerInput(Int32.Parse(lowerBound), Int32.Parse(upperBound),
-                                Int32.Parse(fizz), Int32.Parse(buzz));
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.PromptUser();
+            fizzBuzz.PrintResults();
         }
     }
 }
